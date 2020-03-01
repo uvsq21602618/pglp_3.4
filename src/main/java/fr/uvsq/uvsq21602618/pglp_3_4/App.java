be57347f820f7325dd767df6ff77a6c8c4058c5d
Nathalie BERNARD <nathalie.bernard@ens.uvsq.fr>
2020-03-01 13:56:44 +0100

    creation interface Printer

---
+++ src/main/java/fr/uvsq/uvsq21602618/pglp_3_4/App.java
@@ -0,0 +1,13 @@
+package fr.uvsq.uvsq21602618.pglp_3_4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/fr/uvsq/uvsq21602618/pglp_3_4/Printer.java
@@ -0,0 +1,5 @@
+package fr.uvsq.uvsq21602618.pglp_3_4;
+
+public interface Printer {
+	void print();
+}
